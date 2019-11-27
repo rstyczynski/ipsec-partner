9f953fdcb2f08cb29f369c34e71a04202a0d2b7a
rstyczynski <rstyczynski@gmail.com>
2019-11-27 20:36:45 +0100

    /run filesystem rebuild

--- monitoring/status_update.sh
+++ monitoring/status_update.sh
@@ -7,6 +7,19 @@ PATH=$PATH:/opt/ipsec-partner/bin
 # ipsec
 # 
 
+if [ ! -d /run/ipsec-partner ]; then
+	mkdir -p /run/ipsec-partner/status/ipsec/whack
+	mkdir -p /run/ipsec-partner/status/ip/tunnel
+	chmod -R 755 /run/ipsec-partner/*
+	
+	ln -s /run/ipsec-partner/status /var/www/html/ipsec-partner
+	chcon -R -t httpd_sys_content_t /var/www/html/ipsec-partner
+	chcon -R -t httpd_sys_content_t /var/www/html/ipsec-partner/ip
+	chcon -R -t httpd_sys_content_t /var/www/html/ipsec-partner/ip/tunnel
+	chcon -R -t httpd_sys_content_t /var/www/html/ipsec-partner/ipsec/
+	chcon -R -t httpd_sys_content_t /var/www/html/ipsec-partner/ipsec/whack/
+fi
+
 # add trafficstatus
 ipsec whack --trafficstatus > /run/ipsec-partner/status/ipsec/whack/trafficstatus.raw
 cat /run/ipsec-partner/status/ipsec/whack/trafficstatus.raw | trafficstatus2json.py > /run/ipsec-partner/status/ipsec/whack/trafficstatus
