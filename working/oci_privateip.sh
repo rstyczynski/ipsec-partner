81e55741ad8b84a6e1ac8b42d63be71c6f780da0
rstyczynski <rstyczynski@gmail.com>
2020-03-30 21:12:14 +0200

    OCI pacemaker resource managers

---
+++ working/oci_privateip.sh
@@ -0,0 +1,221 @@
+#!/bin/bash
+action=$1
+
+#
+# http://www.linux-ha.org/doc/dev-guides/ra-dev-guide.html
+#
+
+# Initialization:
+: ${OCF_FUNCTIONS_DIR=${OCF_ROOT}/lib/heartbeat}
+. ${OCF_FUNCTIONS_DIR}/ocf-shellfuncs
+
+DEBUG=1
+
+export LC_ALL=en_US.utf8
+export LANG=en_US.utf8
+export LC_CTYPE=en_US.utf8
+
+
+#
+# functions
+#
+
+# date
+function utc::now() {
+    #date +'%d%m%YT%H%M'
+    date -u +"%Y-%m-%dT%H:%M:%S.000Z"
+}
+
+# logging
+function loginfo {
+    line_pfx="$(utc::now)"
+    echo "$line_pfx;$1" >> /var/log/oci_privateip.log
+}
+
+function logdebug {
+    if [ $DEBUG -eq 1 ];then
+        line_pfx="$(utc::now)"
+        echo "$line_pfx;$1" >> /var/log/oci_privateip.log
+    fi
+}
+
+
+function hasPrivateIp() {
+    oci_private_ip=$1
+    oci_vnic_id=$2
+
+    if [ ! -f /run/oci/private_ip.text ]; then
+        logdebug "hasPrivateIp: Not started"
+        return 1
+    else
+        privateip_id=$(cat /run/oci/private_ip.text)
+
+        # I do not want to cal OCI API too frequently
+        # anyway is started returning:
+        # ServiceError:
+        # {
+        #     "code": "NotAuthorizedOrNotFound", 
+        #     "message": "Either Floating Private IP with ID 10.106.6.251 does not exist or you are not authorized to access it.", 
+        #     "opc-request-id": "8959A789DB594AC4AE463A248B668B39/3477B5A66D550CDF82E6DD75CFB969C7/263413879E35082A03FAF1BB25EC3310", 
+        #     "status": 404
+        # }
+        # oci network private-ip get --private-ip-id $privateip_id > /run/oci/private_ip_get.json 2>/run/oci/private_ip_get.err
+        # assigned_vnic_id=$(cat /run/oci/private_ip_get.json | jq -r '.data["vnic-id"]')
+        # if [ "$assigned_vnic_id" == "$oci_vnic_id" ]; then
+        #     return 0
+        # else
+        #     return 1
+        # fi
+
+        # let's use assign  w/o force option
+        oci network vnic assign-private-ip \
+                --vnic-id $OCF_RESKEY_vnic_id \
+                --ip-address $OCF_RESKEY_ip > /run/oci/private_ip_gentleassign.json 2>/run/oci/private_ip_gentleassign.err
+        if [ $? -eq 0 ] ; then
+            if [ $(stat -c%s /run/oci/private_ip_gentleassign.err) -gt 0 ]; then
+
+                grep "Taking no action as IP address $OCF_RESKEY_ip is already assigned to VNIC $OCF_RESKEY_vnic_id" /run/oci/private_ip_gentleassign.err >/dev/nul 2>&1
+                if [ $? -eq 0 ]; then 
+                    logdebug "hasPrivateIp: Already assigned. Response: $(cat /run/oci/private_ip_gentleassign.json), $(cat /run/oci/private_ip_gentleassign.err)"
+                    return 0
+                else
+
+                    logdebug "hasPrivateIp: Error. Response: $(cat /run/oci/private_ip_gentleassign.json), $(cat /run/oci/private_ip_gentleassign.err)"
+                    return 1
+                fi
+            else
+                logdebug "hasPrivateIp: OK. Response: $(cat /run/oci/private_ip_gentleassign.json), $(cat /run/oci/private_ip_gentleassign.err)"
+                return 0
+            fi
+        else
+            logdebug "hasPrivateIp: Error. Response: $(cat /run/oci/private_ip_gentleassign.json), $(cat /run/oci/private_ip_gentleassign.err)"
+            return 1
+        fi
+
+        # I can always ping assigned IP, even if it's unassigned from OCI side
+        # ping $privateip_id always works ! :(
+
+        # Let's set source IP to $privateip_id and try to cal external service. 
+        # As we are insode of Oracle data center, let's ping oracle.com, but with only one jump
+        # timeout 5 traceroute -s $privateip_id -m  1 oracle.com 2>&1 >/run/oci/hasPrivateIp.out
+        # case $? in
+        # 0)  # all good
+        #     logdebug "hasPrivateIp: OK. Response: $(/run/oci/hasPrivateIp.out)"
+        #     return 0 ;;
+        # 1)  # no IP at the interfaces!
+        #     logdebug "hasPrivateIp: No IP at the interfaces! Response: $(cat /run/oci/hasPrivateIp.out)"
+        #     return 1 ;;
+        # 124) # assigned IP at OS, but not at OCI or total error.
+        #     logdebug "hasPrivateIp: assigned IP at OS, but not at OCI or total error. Response: $(cat /run/oci/hasPrivateIp.out)"
+        #     return 1 ;;
+        # esac  
+    fi
+}
+
+function meta_data() {
+    cat <<EOF
+<?xml version="1.0"?>
+<!DOCTYPE resource-agent SYSTEM "ra-api-1.dtd">
+<resource-agent name="oci_private_ip" version="0.1">
+  <version>0.1</version>
+  <longdesc lang="en">Assigns a secondary private IP address to the specified VNIC. The secondary private IP must be in the same subnet as the VNIC. This command can also be used to move an existing secondary private IP to the specified VNIC.</longdesc>
+  <shortdesc lang="en">Assign PrivateIp via Oracle OCI API</shortdesc>
+
+<parameters>
+
+<parameter name="ip" unique="0" required="1">
+<longdesc lang="en">
+A private IP address of your choice. Must be an available IP address within the subnet's CIDR. If you don't specify a value, Oracle automatically assigns a private IP address from the subnet.
+
+More info: https://docs.cloud.oracle.com/en-us/iaas/tools/oci-cli/2.9.5/oci_cli_docs/cmdref/network/vnic/assign-private-ip.html
+</longdesc>
+
+<shortdesc lang="en">Private IP</shortdesc>
+</parameter>
+
+<parameter name="vnic_id" unique="0" required="1">
+<longdesc lang="en">
+The OCID of the VNIC to assign the private IP to. The VNIC and private IP must be in the same subnet.
+</longdesc>
+<shortdesc lang="en">VNIC id</shortdesc>
+</parameter>
+
+</parameters>
+
+  <actions>
+    <action name="start"        timeout="20" />
+    <action name="stop"         timeout="20" />
+    <action name="monitor"      timeout="20" interval="10" depth="0" />
+    <action name="meta-data"    timeout="5" />
+  </actions>
+
+</resource-agent>
+EOF
+}
+
+umask 077
+mkdir -p /run/oci
+
+case $action in
+
+start)
+    if hasPrivateIp $OCF_RESKEY_ip $OCF_RESKEY_vnic_id; then
+        result=$OCF_SUCCESS
+    else
+        # resource parameters
+        # http://www.linux-ha.org/doc/dev-guides/_api_definitions.html#_environment_variables
+        timeout 15 oci network vnic assign-private-ip \
+            --vnic-id $OCF_RESKEY_vnic_id \
+            --unassign-if-already-assigned \
+            --ip-address $OCF_RESKEY_ip > /run/oci/private_ip_assign.json 2>/run/oci/private_ip_assign.err
+        if [ $? -eq 0 ] ; then
+            # when already assigned responses with null on stdout, and info on stderr and ecode 0. O means ok. Keep IP
+            echo $OCF_RESKEY_ip >/run/oci/private_ip.text
+            result=$OCF_SUCCESS
+            loginfo "oci_privateip: started OK: $result, $(cat /run/oci/private_ip_assign.json), $(cat /run/oci/private_ip_assign.err)"
+        else
+            result=$OCF_ERR_GENERIC
+            loginfo "oci_privateip: started with error: $result, $(cat /run/oci/private_ip_assign.json), $(cat /run/oci/private_ip_assign.err)"
+        fi
+    fi
+    ;;
+stop)
+    if hasPrivateIp $OCF_RESKEY_ip $OCF_RESKEY_vnic_id; then
+        # resource parameters
+        # http://www.linux-ha.org/doc/dev-guides/_api_definitions.html#_environment_variables
+        timeout 15 oci network vnic unassign-private-ip \
+            --vnic-id $OCF_RESKEY_vnic_id \
+            --ip-address $OCF_RESKEY_ip > /run/oci/private_ip_unassign.json 2>/run/oci/private_ip_unassign.err
+        if [ $? -eq 0 ] ; then
+            \rm -f /run/oci/private_ip_assign.json
+            result=$OCF_SUCCESS
+            loginfo "oci_privateip: stopped OK: $result, $(cat /run/oci/private_ip_unassign.json), $(cat /run/oci/private_ip_unassign.err)"
+        else
+            result=$OCF_ERR_GENERIC
+            loginfo "oci_privateip: stopped with error: $result, $(cat /run/oci/private_ip_unassign.json), $(cat /run/oci/private_ip_unassign.err)"
+        fi
+    fi
+    result=$OCF_SUCCESS
+    ;;
+meta-data)
+    meta_data
+    result=$OCF_SUCCESS
+    ;;
+status | monitor)
+    # resource parameters
+    # http://www.linux-ha.org/doc/dev-guides/_api_definitions.html#_environment_variables
+    if hasPrivateIp $OCF_RESKEY_ip $OCF_RESKEY_vnic_id; then
+        result=$OCF_SUCCESS
+    else
+        result=$OCF_NOT_RUNNING
+    fi
+    ;;
+*)
+    logdebug "No such action: $action"
+    result=$OCF_ERR_UNIMPLEMENTED
+    ;;
+esac
+rc=$result
+
+ocf_log debug "${OCF_RESOURCE_INSTANCE} $action returned $rc"
+exit $rc
---
+++ working/oci_publicip.sh
@@ -0,0 +1,232 @@
+#!/bin/bash
+action=$1
+
+#
+# http://www.linux-ha.org/doc/dev-guides/ra-dev-guide.html
+#
+
+# Initialization:
+: ${OCF_FUNCTIONS_DIR=${OCF_ROOT}/lib/heartbeat}
+. ${OCF_FUNCTIONS_DIR}/ocf-shellfuncs
+
+DEBUG=1
+
+export LC_ALL=en_US.utf8
+export LANG=en_US.utf8
+export LC_CTYPE=en_US.utf8
+
+
+#
+# functions
+#
+
+# date
+function utc::now() {
+    #date +'%d%m%YT%H%M'
+    date -u +"%Y-%m-%dT%H:%M:%S.000Z"
+}
+
+# logging
+function loginfo {
+    line_pfx="$(utc::now)"
+    echo "$line_pfx;$1" >> /var/log/oci_publicip.log
+}
+
+function logdebug {
+    if [ $DEBUG -eq 1 ];then
+        line_pfx="$(utc::now)"
+        echo "$line_pfx;$1" >> /var/log/oci_publicip.log
+    fi
+}
+
+function getVNICIp_id() {
+    vnic_no=$1
+
+    call_id=private_ip_curl
+    timeout 5 curl -s -L http://169.254.169.254/opc/v1/vnics/ > /run/oci/$call_id.out 2> /run/oci/$call_id.err
+    if [ $? -ne 0 ]; then
+        loginfo "getVNICIp_id: Error. $(cat /run/oci/$call_id.out), $(cat /run/oci/$call_id.err)"
+        return 1
+    else
+        loginfo "getVNICIp_id: OK. $(cat /run/oci/$call_id.out), $(cat /run/oci/$call_id.err)"
+        vnic_id=$(cat /run/oci/$call_id.out | jq -r .[$vnic_no].vnicId)
+    fi
+
+    call_id=private_ip_list
+    timeout 5 oci network private-ip list --vnic-id $vnic_id > /run/oci/$call_id.out 2> /run/oci/$call_id.err
+    if [ $? -ne 0 ]; then
+        loginfo "getVNICIp_id: Error. $(cat /run/oci/$call_id.out), $(cat /run/oci/$call_id.err)"
+        return 1
+    else
+        privateIp_id=$(cat /run/oci/$call_id.out | 
+        sed 's/is-primary/is_primary/g' | 
+        jq -r '.data[] | select(.is_primary == true) | .id')
+        loginfo "getVNICIp_id: OK. $(cat /run/oci/$call_id.out), $(cat /run/oci/$call_id.err)"
+    fi
+
+    echo $privateIp_id
+}
+
+function getPublicIp_id() {
+    publicIp_id=$1
+
+    call_id=public_ip_get
+    timeout 5 oci network public-ip get --public-ip-address  $publicIp_id >/run/oci/$call_id.out 2> /run/oci/$call_id.err
+    if [ $? -ne 0 ]; then
+        loginfo "getPublicIp_id: Error. $(cat /run/oci/$call_id.out), $(cat /run/oci/$call_id.err)"
+        return 1
+    fi
+
+    cat /run/oci/$call_id.out | jq -r .data.id
+}
+
+function hasPublicIp() {
+    publicIp_id=$1
+
+    logdebug "hasPublicIp: public-ip get started"
+    timeout 5 oci network public-ip get --public-ip-address $publicIp_id > /run/oci/public_ip_get.json 2>/run/oci/public_ip_get.err
+    result=$?
+    if [ $result -eq 0 ]; then
+        logdebug "hasPublicIp: looking for entity_type"
+        entity_type=$(cat /run/oci/public_ip_get.json | sed 's/assigned-entity-type/assigned_entity_type/g' | jq -r '.data.assigned_entity_type')
+
+        case $entity_type in
+        PRIVATE_IP)
+            entity_id=$(cat /run/oci/public_ip_get.json | sed 's/assigned-entity-id/assigned_entity_id/g' | jq -r '.data.assigned_entity_id')
+
+            # so here we have private ip OCID
+            if [ "$entity_id" == "$oci_vnic_Ip_id" ]; then
+                logdebug "hasPublicIp: OK. Response: $(cat /run/oci/public_ip_get.json), $(cat /run/oci/public_ip_get.err)"
+                return 0
+            else
+                logdebug "hasPublicIp: Assigned to other. Response: $(cat /run/oci/public_ip_get.json), $(cat /run/oci/public_ip_get.err)"
+                return 1
+            fi
+            ;;
+        null)
+            logdebug "hasPublicIp: Not assigned. Response: $(cat /run/oci/public_ip_get.json), $(cat /run/oci/public_ip_get.err)"
+            return 1
+            ;;
+        *)
+            logdebug "hasPublicIp: Error. Not supported entity_type reported. Response: $(cat /run/oci/public_ip_get.json), $(cat /run/oci/public_ip_get.err)"
+            return 1
+            ;;
+        esac
+        logdebug "hasPublicIp: entity_type processed"
+    else
+        logdebug "hasPublicIp: General error. Response: $(cat /run/oci/public_ip_get.json), $(cat /run/oci/public_ip_get.err)"
+        return 1
+    fi
+}
+
+function meta_data() {
+    cat <<EOF
+<?xml version="1.0"?>
+<!DOCTYPE resource-agent SYSTEM "ra-api-1.dtd">
+<resource-agent name="oci_public_ip" version="0.1">
+  <version>0.1</version>
+  <longdesc lang="en">
+  Assigns the specified public IP to a netowrk card.
+  
+  More info: https://docs.cloud.oracle.com/en-us/iaas/tools/oci-cli/2.9.2/oci_cli_docs/cmdref/network/public-ip/update.html
+  </longdesc>
+  <shortdesc lang="en">Assign PublicIp via Oracle OCI API</shortdesc>
+
+<parameters>
+
+<parameter name="publicIp" unique="0" required="1">
+<longdesc lang="en">
+Reserved public IP address created in OCI.
+</longdesc>
+<shortdesc lang="en">publicIp</shortdesc>
+</parameter>
+
+<parameter name="vnic_no" unique="0" required="1">
+<longdesc lang="en">
+xxx
+</longdesc>
+<shortdesc lang="en">vnic no</shortdesc>
+</parameter>
+
+</parameters>
+
+  <actions>
+    <action name="start"        timeout="20" />
+    <action name="stop"         timeout="20" />
+    <action name="monitor"      timeout="20" interval="10" depth="0" />
+    <action name="meta-data"    timeout="5" />
+  </actions>
+
+</resource-agent>
+EOF
+}
+
+umask 077
+mkdir -p /run/oci
+
+if [ ! "$action" == "meta-data" ]; then 
+    loginfo "oci_public_ip: Initializing: $action, $OCF_RESKEY_publicIp, $OCF_RESKEY_vnic_no"
+    # moved to start, as it's not changing
+    oci_publicIp_id=$(getPublicIp_id $OCF_RESKEY_publicIp)
+    oci_vnic_Ip_id=$(getVNICIp_id $OCF_RESKEY_vnic_no)
+    loginfo "oci_public_ip: Initialized for: $oci_publicIp_id on $oci_vnic_Ip_id"
+fi
+
+case $action in
+start)
+    if hasPublicIp $OCF_RESKEY_publicIp; then
+        result=$OCF_SUCCESS
+    else
+        call_id=rm_start_public_ip_get
+        timeout 5 oci network public-ip update \
+        --public-ip-id $oci_publicIp_id \
+        --private-ip-id $oci_vnic_Ip_id \
+        --force >/run/oci/$call_id.out 2> /run/oci/$call_id.err
+        if [ $? -ne 0 ]; then
+            loginfo "rm_start: Error. $(cat /run/oci/$call_id.out), $(cat /run/oci/$call_id.err)"
+            result=$OCF_ERR_GENERIC
+        else
+            loginfo "rm_start: Assigned. $(cat /run/oci/$call_id.out), $(cat /run/oci/$call_id.err)"
+            result=$OCF_SUCCESS
+        fi
+    fi
+    ;;
+stop)
+    if hasPublicIp $OCF_RESKEY_publicIp; then
+
+        call_id=rm_stop_public_ip_get
+        timeout 5 oci network public-ip update \
+        --public-ip-id $oci_publicIp_id \
+        --private-ip-id '' >/run/oci/$call_id.out 2> /run/oci/$call_id.err
+        if [ $? -ne 0 ]; then
+            loginfo "rm_stop: Error. $(cat /run/oci/$call_id.out), $(cat /run/oci/$call_id.err)"
+            result=$OCF_ERR_GENERIC
+        else
+            loginfo "rm_stop: Unassigned. $(cat /run/oci/$call_id.out), $(cat /run/oci/$call_id.err)"
+            result=$OCF_SUCCESS
+        fi
+    fi
+    result=$OCF_SUCCESS
+    ;;
+meta-data)
+    meta_data
+    result=$OCF_SUCCESS
+    ;;
+status | monitor)
+    # resource parameters
+    # http://www.linux-ha.org/doc/dev-guides/_api_definitions.html#_environment_variables
+    if hasPublicIp $OCF_RESKEY_publicIp; then
+        result=$OCF_SUCCESS
+    else
+        result=$OCF_NOT_RUNNING
+    fi
+    ;;
+*)
+    loginfo "No such action: $action"
+    result=$OCF_ERR_UNIMPLEMENTED
+    ;;
+esac
+rc=$result
+
+ocf_log debug "${OCF_RESOURCE_INSTANCE} $action returned $rc"
+exit $rc
