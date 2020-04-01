45d6f55c60b5736bc3287783c3bc7e2142c74162
rstyczynski <rstyczynski@gmail.com>
2020-04-01 12:53:14 +0200

    stonith, etc.

--- working/pacemaker_cluster_init.sh
+++ working/pacemaker_cluster_init.sh
@@ -34,6 +34,12 @@ pcs cluster auth $cluster_nodes -u hacluster -p $pass --force
 
 pcs cluster destroy --force --name ipsec_cluster $cluster_nodes 
 pcs cluster setup --force --name ipsec_cluster $cluster_nodes
+
+pcs property set stonith-enabled=false
+pcs property set no-quorum-policy=ignore
+pcs resource defaults resource-stickiness=100
+pcs resource defaults migration-threshold=5
+
 pcs cluster start --all
 
 pcs status
